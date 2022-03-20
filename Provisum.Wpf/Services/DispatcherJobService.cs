8d0cc59f2c0cffc86cb68cc53b2844cb0707fae8
Connor Haigh <connorhaigh@gmail.com>
2022-03-20 11:15:01 +0000

    Added job service for submitting units of work

---
+++ Provisum.Wpf/Services/DispatcherJobService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using Provisum.Services;
+
+namespace Provisum.Wpf.Services
+{
+	/// <summary>
+	/// Represents a dispatcher-based job service.
+	/// </summary>
+	public sealed class DispatcherJobService : IJobService
+	{
+		/// <summary>
+		/// Creates a new dispatcher job service instance for the dispatcher attached to the current application.
+		/// </summary>
+		/// <returns>The instance.</returns>
+		public static DispatcherJobService CreateForCurrentApplicationDispatcher() => new DispatcherJobService(Application.Current.Dispatcher);
+
+		/// <summary>
+		/// Creates a new dispatcher job service instance with the specified dispatcher.
+		/// </summary>
+		/// <param name="dispatcher">The dispatcher.</param>
+		public DispatcherJobService(Dispatcher dispatcher)
+		{
+			this.dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
+		}
+
+		/// <inheritdoc />
+		public void Submit(Action action) => this.dispatcher.Invoke(action);
+
+		private readonly Dispatcher dispatcher = null;
+	}
+}
---
+++ Provisum/Services/JobService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Provisum.Services
+{
+	/// <summary>
+	/// Represents a job service, for submitting individual units of work.
+	/// </summary>
+	public interface IJobService : IService
+	{
+		/// <summary>
+		/// Submits the specified action.
+		/// </summary>
+		/// <param name="action">The action.</param>
+		void Submit(Action action);
+	}
+}
