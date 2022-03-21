32be5f4e3c9f9b70bb2c3d4ae401f5d1e8c5af30
Connor Haigh <connorhaigh@gmail.com>
2022-03-21 17:26:08 +0000

    Added list swap extension method

---
+++ Provisum.Tests/Extensions/ListExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Provisum.Extensions;
+
+namespace Provisum.Tests.Extensions
+{
+	[TestClass]
+	public sealed class ListExtensionsTests
+	{
+		[TestMethod]
+		public void TestSwap()
+		{
+			var array = new string[] { "Foo", "Bar", "Baz" };
+			array.Swap(0, 2);
+
+			Assert.IsTrue(array.SequenceEqual(new string[] { "Baz", "Bar", "Foo" }));
+		}
+	}
+}
---
+++ Provisum/Extensions/ListExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Provisum.Extensions
+{
+	/// <summary>
+	/// Provides <see cref="IList{T}" /> extension methods.
+	/// </summary>
+	public static class ListExtensions
+	{
+		/// <summary>
+		/// Swaps the element at the first specified index with the element at the second specified index.
+		/// </summary>
+		/// <typeparam name="T">The list type.</typeparam>
+		/// <param name="list">The list.</param>
+		/// <param name="a">The first index.</param>
+		/// <param name="b">The second index.</param>
+		/// <returns>The swapped element.</returns>
+		public static T Swap<T>(this IList<T> list, int a, int b)
+		{
+			if (list == null)
+			{
+				throw new ArgumentNullException(nameof(list));
+			}
+
+			if (a < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(a), "First index cannot be less than zero.");
+			}
+
+			if (b < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(a), "Second index cannot be less than zero.");
+			}
+
+			if (a >= list.Count)
+			{
+				throw new ArgumentOutOfRangeException(nameof(a), "First index cannot be greater than the size of the list.");
+			}
+
+			if (b >= list.Count)
+			{
+				throw new ArgumentOutOfRangeException(nameof(a), "Second index cannot be greater than the size of the list.");
+			}
+
+			var element = list[a];
+
+			list[a] = list[b];
+			list[b] = element;
+
+			return element;
+		}
+	}
+}
