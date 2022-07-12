ec61e090cd4a9c4ab1841ed84fd3c549b1b5514a
Connor Haigh <connorhaigh@gmail.com>
2022-07-12 11:42:16 +0100

    Added notification service and removed timer service

---
+++ Provisum/Services/NotificationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provisum.Services
+{
+	/// <summary>
+	/// Represents a notification service, for displaying system notifications.
+	/// </summary>
+	public interface INotificationService
+	{
+		/// <summary>
+		/// Shows a notification with the specified title and specified message.
+		/// </summary>
+		/// <param name="title">The title.</param>
+		/// <param name="message">The message.</param>
+		void Show(string title, string message);
+	}
+}
